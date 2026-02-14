44ec5eaab37d5316195c155d16c9415990bd5667
SubhajitMitra-GH <msubhojit182004@gmail.com>
2026-02-14 23:37:54 +0530

    check

--- scripts/temp.py
+++ scripts/temp.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("data/RAW/CBIS-DDSM/csv/mass_case_description_train_set.csv")
 print(df["crowapped image file path"].iloc[0])
-print(df["crowapped image file path"].iloc[0])
+
