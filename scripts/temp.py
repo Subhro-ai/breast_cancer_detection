aa1e430996d5ff78431d8affe4320c31225bb42d
SubhajitMitra-GH <amegcontact@gmail.com>
2026-02-14 23:30:42 +0530

    added model

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv("data/RAW/CBIS-DDSM/csv/mass_case_description_train_set.csv")
-print(df["cropped image file path"].iloc[0])
+print(df["crowapped image file path"].iloc[0])
+print(df["crowapped image file path"].iloc[0])
